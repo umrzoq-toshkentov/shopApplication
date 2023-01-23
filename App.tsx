161709a4430ea37b1fbc2d2f7433813cfb0b960d
UmrzoqScope3 <utoshkentov@scope3.com>
2023-01-23 18:11:50 +0500

    feat: bottom tab navigator through screens

--- App.tsx
+++ App.tsx
@@ -16,6 +16,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SCREENS } from './src/constants/screens';
 import { colors } from './src/utils/colors';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from './src/screens/app/Home';
+import { Profile } from './src/screens/app/Profile';
+import { Favorites } from './src/screens/app/Favorites';
+import { Image, ImageProps, ImageSourcePropType } from 'react-native';
 
 const MyTheme = {
   ...DefaultTheme,
@@ -27,8 +32,34 @@ const MyTheme = {
 };
 
 const Stack = createNativeStackNavigator()
+const BottomTab = createBottomTabNavigator();
+const TAB = () => {
+  return (
+    <BottomTab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => {
+        let iconName;
+        if (route.name === SCREENS.HOME) {
+          iconName = focused ? require('./src/assets/tabs/home_active.png') : require('./src/assets/tabs/home.png')
+        } else if (route.name === SCREENS.PROFILE) {
+          iconName = focused ? require('./src/assets/tabs/profile_active.png') : require('./src/assets/tabs/profile.png')
+        } else if (route.name === SCREENS.FAVORITES) {
+          iconName = focused ? require('./src/assets/tabs/bookmark_active.png') : require('./src/assets/tabs/bookmark.png')
+        }
+
+        return <Image style={{ width: 24, height: 24 }} source={iconName} />
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+    })}>
+      <BottomTab.Screen name={SCREENS.HOME} component={Home} />
+      <BottomTab.Screen name={SCREENS.PROFILE} component={Profile} />
+      <BottomTab.Screen name={SCREENS.FAVORITES} component={Favorites} />
+    </BottomTab.Navigator>
+  )
+}
 
 function App(): JSX.Element {
+  const isSigned = true
 
   useEffect(() => {
     GoogleSignin.configure({
@@ -43,9 +74,19 @@ function App(): JSX.Element {
     <SafeAreaProvider>
       <NavigationContainer theme={MyTheme}>
         <Stack.Navigator>
-          <Stack.Screen options={{ headerShown: false }} name={SCREENS.SPLASH} component={Splash} />
-          <Stack.Screen options={{ headerShown: false }} name={SCREENS.SIGN_UP} component={SignUp} />
-          <Stack.Screen options={{ headerShown: false }} name={SCREENS.SIGN_IN} component={SignIn} />
+          {
+            isSigned ? (
+              <>
+                <Stack.Screen options={{ headerShown: false }} name={SCREENS.TABS} component={TAB} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen options={{ headerShown: false }} name={SCREENS.SPLASH} component={Splash} />
+                <Stack.Screen options={{ headerShown: false }} name={SCREENS.SIGN_UP} component={SignUp} />
+                <Stack.Screen options={{ headerShown: false }} name={SCREENS.SIGN_IN} component={SignIn} />
+              </>
+            )
+          }
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
